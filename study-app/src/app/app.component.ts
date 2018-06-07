3a577a9c5cce5c3768b2f824b492d27094fd0443
Roman oliynyk <oliynykroma@live.com>
2018-06-07 16:40:54 +0300

    basic styles

--- study-app/src/app/app.component.ts
+++ study-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'used interpolation title of the page';
+  subtitle = 'this is may subtitle';
 }
